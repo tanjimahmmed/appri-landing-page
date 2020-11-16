835881c1dd21fa9d87a56c6a46f853cacac6e81a
tanjimahmmed <tanjimahmmed.8@gmail.com>
2020-11-16 22:34:28 +0600

    add more section
    add more section

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,5 +8,27 @@
         time: 1000
     });
 
+    // modal video
+    $(".popup_video").modalVideo();
+    $(".popup_video").on('click', function () {
+        return false;
+    });
+
+    $('.testimonial_active').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.test_img_active'
+      });
+      $('.test_img_active').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.testimonial_active',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,
+        centerPadding: '0px'
+      });
     
 })(jQuery);
